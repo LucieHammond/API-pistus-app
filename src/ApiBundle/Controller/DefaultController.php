68d6ae12d9eb6f2456c2a752ea32e625af067cbc
Tom Brendlé <tom.brendle@student.ecp.fr>
2016-02-06 20:35:37 +0100

    Stats

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -41,4 +41,32 @@ class DefaultController extends Controller
         $response->setData($data);
         return $response;
     }
+
+    /**
+     * @Route("/ranking/{authKey}", name="api_ranking")
+     */
+    public function rankingAction(User $user){
+        $keys = ['maxSpeed', 'altMax', 'kmSki', 'skiTime'];
+        $em = $this->getDoctrine()->getManager();
+        $data = array();
+        foreach ($keys as $key) {
+            $func = "get" . ucfirst($key);
+            $data[$key]=array();
+            $data[$key]['count'] = intval($em->createQuery(
+                "SELECT count(u.id) 
+                FROM ApiBundle:User u 
+                WHERE u.".$key." > :".$key
+            )->setParameter($key, $user->$func())->getSingleResult()['1'])+1;
+            $data[$key]['ranking'] = $em->createQuery(
+                "SELECT u 
+                FROM ApiBundle:User u 
+                ORDER BY u." . $key 
+            )->setMaxResults(5)->getResult();
+        }
+        $response = new JsonResponse();
+        $response->setData(array(
+            'data' => $data
+        ));
+        return $response;
+    }
 }
