1f92e0ee7cb3bf5f59e958fbf76b2e408e32c294
Tom Brendlé <tom.brendle@student.ecp.fr>
2016-02-16 19:28:35 +0100

    general info

--- src/ApiBundle/Controller/NewsController.php
+++ src/ApiBundle/Controller/NewsController.php
@@ -33,20 +33,86 @@ class NewsController extends Controller
             JOIN ApiBundle:User u WITH u.authKey = :ak
             JOIN u.targets t
             WHERE n.target = t.name
-            ORDER BY n.date ASC"
+            ORDER BY n.date DESC"
         )->setParameter('ak', $user->getAuthKey())->getResult();
         $generalNews = $em->createQuery(
             "SELECT DISTINCT n
-            FROM ApiBundle:News n WHERE n.target = 'all' ORDER BY n.date ASC")->getResult();
-        $globalInfo = $em->createQuery(
+            FROM ApiBundle:News n WHERE n.target = 'all' ORDER BY n.date DESC")->getResult();
+        $generalInfos = $em->createQuery(
             "SELECT DISTINCT n
-            FROM ApiBundle:News n WHERE n.target = 'info' ORDER BY n.date ASC")->getResult();
-
+            FROM ApiBundle:GeneralInfo n")->getResult();
+        $gi = array();
+        //Foutu pour foutu...
+        foreach ($generalInfos as $key => $info) {
+            $gi[$info->getParent()]=array("order"=>$info->getZorder(), "data"=>array());
+        }
+        foreach ($generalInfos as $key => $info) {
+            $gi[$info->getParent()]['parent']= $info->getParent();
+            $gi[$info->getParent()]["order"]=min($info->getZorder(), $gi[$info->getParent()]["order"]);
+            $gi[$info->getParent()]["data"][]=$info;
+        }
+        $gni = array();
+        foreach ($gi as $key => $value) {
+            $nvalue = array();
+            foreach ($value["data"] as $k => $v) {
+                $nvalue[$v->getZorder()]=$v;
+            }
+            krsort($nvalue);
+            $gni[$value["order"]]=array("title"=> $value["parent"], "infos"=>array_values($nvalue));
+            
+        }
+        krsort($gni);
         $response = new JsonResponse();
         $response->setData(array(
             'myNews' => $myNews,
             'generalNews' => $generalNews,
-            'generalInfo'=>$globalInfo
+            'generalInfo' => $gni
+        ));
+        return $response;
+    }
+
+    /**
+     * Lists all News entities.
+     *
+     * @Route("/my", name="news_index_my")
+     * @Method("GET")
+     */
+    public function indexMyAction(User $user)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $myNews = $em->createQuery(
+            "SELECT DISTINCT n
+            FROM ApiBundle:News n
+            JOIN ApiBundle:User u WITH u.authKey = :ak
+            JOIN u.targets t
+            WHERE n.target = t.name
+            ORDER BY n.date DESC"
+        )->setParameter('ak', $user->getAuthKey())->getResult();
+
+        $response = new JsonResponse();
+        $response->setData(array(
+            'myNews' => $myNews
+        ));
+        return $response;
+    }
+
+    /**
+     * Lists all News entities.
+     *
+     * @Route("/general", name="news_index_general")
+     * @Method("GET")
+     */
+    public function indexGeneralAction(User $user)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $generalNews = $em->createQuery(
+            "SELECT DISTINCT n
+            FROM ApiBundle:News n WHERE n.target = 'all' ORDER BY n.date DESC")->getResult();
+
+        $response = new JsonResponse();
+        $response->setData(array(
+            'generalNews' => $generalNews
         ));
         return $response;
     }
